904645b48d29ac11512e918a5e8d54b9a153b871
TGass <tilmann.gass@googlemail.com>
2015-02-28 11:12:25 +0100

    Refactoring parsers 2

--- src/parsers/CommandMsgParser.hs
+++ src/parsers/CommandMsgParser.hs
@@ -1,24 +1,3 @@
-
-{-
-
-<BLOCK_START><command identifier><BLOCK_SEPARATOR>
-
-This will be followed by:
-
-<command code><BLOCK_SEPARATOR><command output><BLOCK_END>
-
-#define BLOCK_START 21 /* '\U' */
-#define BLOCK_SEPARATOR 22 /* '\V' */
-#define BLOCK_END 23 /* '\W' */
-#define BLOCK_POSE_START 24 /* \X */
-#define BLOCK_POSE_END 25 /* \Y */
-
--}
-
-{-
-CommandMessage {commandId = 3, commandCode = 80, message = "You are now observing game 157.Game 157: IMUrkedal (2517) GMRomanov (2638) unrated standard 120 0<12> -------- -pp-Q--- pk------ ----p--- -P---p-- --qB---- -------- ---R-K-- B -1 0 0 0 0 9 157 IMUrkedal GMRomanov 0 120 0 18 14 383 38 57 K/e1-f1 (0:03) Kf1 0 0 0"}
--}
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module CommandMsgParser (
@@ -34,135 +13,82 @@ import GamesParser
 import MoveParser2
 import GameResultParser
 
-import Control.Applicative ((<*>), (*>), (<*), (<$>), (<|>), pure)
+import Control.Applicative ((*>), (<|>), pure)
 
 import Data.Attoparsec.ByteString.Char8
-import qualified Data.Attoparsec.ByteString.Char8 as A (take, takeWhile)
 import qualified Data.ByteString.Char8 as BS
 import Data.Char
-import Data.Conduit
-import qualified Data.Conduit.Binary as CB
-import qualified Data.Conduit.List as CL
-import qualified Data.List as List (filter)
-import Data.Maybe (fromMaybe)
 
-import Network (connectTo, PortID (..))
-import System.IO
 import Data.List.Split (splitOn)
-
 
 
 parseCommandMsg :: BS.ByteString -> Either String CommandMsg
 parseCommandMsg str = parseOnly parser str where
-  parser = choice [
-                    parseMoveMsg
-                  , confirmMoveMsg
-
-                  , observeMsg
-                  , gamesMsg
-                  , soughtMsg
-
-                  , acceptGameMsg
-                  , matchMsg
-
-                  , parseGameResult
-
-                  , parseLogin
-                  , parseAcknoledge
-                  , parseSettingsDone
-                  , parsePassword
-                  , parseGuestLogin
-                  , parseUnkownUsername
-                  , parseInvalidPassword
-                  , parseLoggedIn
-                  , parsePrompt
-
+  parser = choice [ sought
+                  , games
+                  , observe
+                  , accept
+                  , playSuccess
+                  , move'
+                  , confirmMove
+                  , match'
+                  , login
+                  , password
+                  , guestLogin
+                  , unknownUsername
+                  , loggedIn
+                  , invalidPassword
+                  , prompt
+                  , acknoledge
+                  , settingsDone
                   ]
 
-soughtMsg :: Parser CommandMsg
-soughtMsg = commandHead 157 >> soughtList' >>= return . Sought
 
+sought = commandHead 157 >> soughtList' >>= return . Sought
 
-gamesMsg :: Parser CommandMsg
-gamesMsg = commandHead 43 >> paresGamesList >>= return . Games
+games = commandHead 43 >> paresGamesList >>= return . Games
 
+observe = commandHead 80 >> move'' >>= return . Observe
 
-observeMsg :: Parser CommandMsg
-observeMsg = commandHead 80 >> findAndParseMove >>= return . Observe
+accept = commandHead 11 >> move'' >>= return . Accept
 
+playSuccess = commandHead 1111111 >> move'' >>= return . PlaySuccess
 
-acceptGameMsg :: Parser CommandMsg
-acceptGameMsg = commandHead 11 >> findAndParseMove >>= return . Accept
+move' = parseMove >>= return . CommandMsg.Move
 
+confirmMove = commandHead 1 >> move'' >>= return . ConfirmMove
 
-playSuccessMsg :: Parser CommandMsg
-playSuccessMsg = commandHead 1111111 >> findAndParseMove >>= return . PlaySuccess
-
-
-confirmMoveMsg :: Parser CommandMsg
-confirmMoveMsg = commandHead 1 >> findAndParseMove >>= return . ConfirmMove
-
-
-findAndParseMove :: Parser Move
-findAndParseMove = takeTill (== '<') >> parseMove >>= return
-
-
--- | ie: {Game 537 (GuestWSHB vs. GuestNDKP) Creating unrated blitz match.}
-matchMsg :: Parser CommandMsg
-matchMsg = do
+match' :: Parser CommandMsg
+match' = do
   "{Game "
   id <- decimal
   manyTill anyChar "}"
   return $ Match id
 
+login = "login: " >> return Login
 
-parseMoveMsg :: Parser CommandMsg
-parseMoveMsg = parseMove >>= return . CommandMsg.Move
+password = "password: " >> return Password
 
+guestLogin = "Press return to enter the server as \"" >>  manyTill anyChar "\":" >>= return . GuestLogin
 
-parseLogin :: Parser CommandMsg
-parseLogin = "login: " >> return Login
+unknownUsername = "\"" >> manyTill anyChar "\" is not a registered name." >>= return . UnkownUsername
 
+loggedIn = "**** Starting FICS session as " >> manyTill anyChar " ****" >>=
+                 return . LoggedIn . Prelude.head . splitOn "(" -- | Beware the guest handles: ie GuestXWLW(U)
 
-parsePassword :: Parser CommandMsg
-parsePassword = "password: " >> return Password
+invalidPassword = "**** Invalid password! ****" >> return InvalidPassword
 
+prompt = "fics% " >> return Prompt
 
-parseGuestLogin :: Parser CommandMsg
-parseGuestLogin = do
-  "Press return to enter the server as \""
-  name <- manyTill anyChar "\":"
-  return $ GuestLogin name
+acknoledge = commandHead 519 >> (char $ chr 23) >> return Acknoledge
 
-
-parseUnkownUsername :: Parser CommandMsg
-parseUnkownUsername = do
-  "\""
-  name <- manyTill anyChar "\" is not a registered name."
-  return $ UnkownUsername name
+settingsDone = (char $ chr 23) >> return SettingsDone
 
 
--- | Beware the guest handles: ie GuestXWLW(U)
-parseLoggedIn :: Parser CommandMsg
-parseLoggedIn = do
-  "**** Starting FICS session as "
-  name <- manyTill anyChar " ****"
-  return $ LoggedIn (Prelude.head $ splitOn "(" name)
+{- HELPER -}
 
-
-parseInvalidPassword :: Parser CommandMsg
-parseInvalidPassword = "**** Invalid password! ****" >> return InvalidPassword
-
-
-parsePrompt :: Parser CommandMsg
-parsePrompt = "fics% " >> return Prompt
-
-
-parseAcknoledge :: Parser CommandMsg
-parseAcknoledge = commandHead 519 >> (char $ chr 23) >> return Acknoledge
-
-
-parseSettingsDone = (char $ chr 23) >> return SettingsDone
+move'' :: Parser Move
+move'' = takeTill (== '<') >> parseMove >>= return
 
 
 commandHead :: Int -> Parser CommandHead
@@ -175,8 +101,9 @@ commandHead code = do
   return $ CommandHead id
 
 
--- test data
+{- TEST DATA -}
+
 playMsg = BS.pack "Creating: GuestCCFP (++++) GuestGVJK (++++) unrated blitz 0 20 {Game 132 (GuestCCFP vs. GuestGVJK) Creating unrated blitz match.} <12> rnbqkbnr pppppppp ———— ———— ———— ———— PPPPPPPP RNBQKBNR W -1 1 1 1 1 0 132 GuestCCFP GuestGVJK -1 0 20 39 39 10 10 1 none (0:00) none 1 0 0"
 matchMsgS = BS.pack "{Game 537 (GuestWSHB vs. GuestNDKP) Creating unrated blitz match.}"
 obs = BS.pack "You are now observing game 157.Game 157: IMUrkedal (2517) GMRomanov (2638) unrated standard 120 0<12> -------- -pp-Q--- pk------ ----p--- -P---p-- --qB---- -------- ---R-K-- B -1 0 0 0 0 9 157 IMUrkedal GMRomanov 0 120 0 18 14 383 38 57 K/e1-f1 (0:03) Kf1 0 0 0"
-guestLogin = BS.pack $ "Press return to enter the server as \"FOOBAR\":"
+guestLoginTxt = BS.pack $ "Press return to enter the server as \"FOOBAR\":"
