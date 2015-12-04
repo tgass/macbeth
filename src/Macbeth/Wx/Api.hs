930c74631aff662010105eaa1e5c34ce55bac04e
TGass <tilmann.gass@googlemail.com>
2015-12-04 21:55:58 +0100

    show PieceMove simplified. No extra castling cases needed.

--- src/Macbeth/Wx/Api.hs
+++ src/Macbeth/Wx/Api.hs
@@ -1,7 +1,10 @@
 module Macbeth.Wx.Api (
-  PieceSet(..),
   BoardState(..),
-  DraggedPiece(..)
+  DraggedPiece(..),
+  PieceMove (..),
+  movePiece,
+  movePieces,
+  PieceSet(..)
 ) where
 
 import Macbeth.Api.Api
@@ -9,9 +12,6 @@ import Macbeth.Api.Move
 import Macbeth.Api.Game
 
 import Graphics.UI.WX
-
-
-data PieceSet = PieceSet { path :: FilePath, display :: String }
 
 data BoardState = BoardState { lastMove :: Move
                              , gameResult :: Maybe GameResult
@@ -29,3 +29,17 @@ data DraggedPiece = DraggedPiece { _point :: Point
                                  , _piece :: Piece
                                  , _square :: Square } deriving (Show)
 
+data PieceMove = PieceMove { piece :: Piece, from :: Square, to :: Square }
+
+instance Show PieceMove where
+  show (PieceMove _ s1 s2) = show s1 ++ show s2
+
+movePiece :: PieceMove -> Position -> Position
+movePiece (PieceMove piece from to) position =
+  filter (\(s, _) -> s /= from && s /= to) position ++ [(to, piece)]
+
+movePieces :: [PieceMove] -> Position -> Position
+movePieces [] pos = pos
+movePieces (x:xs) pos = movePieces xs (movePiece x pos)
+
+data PieceSet = PieceSet { path :: FilePath, display :: String }
