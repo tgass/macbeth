c8b136362b8a62d35a4255bb6080b5648d5eecbc
TGass <tilmann.gass@googlemail.com>
2016-05-18 22:01:05 +0200

    newtype GameId

--- src/Macbeth/Fics/Parsers/GamesParser.hs
+++ src/Macbeth/Fics/Parsers/GamesParser.hs
@@ -15,9 +15,6 @@ import Data.Attoparsec.ByteString.Char8
 parseGamesList :: Parser [Game]
 parseGamesList = many' gameP
 
-gameId' :: Parser GameId
-gameId' = GameId <$> decimal
-
 gameP :: Parser Game
 gameP = Game
   <$> (takeTill (== '\n') *> "\n" *> many space *> Api.gameId)
