c18c45e417a1d7cf16ecbbb8fa012a9929e64c05
TGass <tilmann.gass@googlemail.com>
2020-09-05 16:51:56 +0200

    Adding usage documentation for env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# FIXME This is hack for https://github.com/commercialhaskell/stack/issues/2299
+
+# This is hack for https://github.com/commercialhaskell/stack/issues/2299
+# Usage: ./env.sh stack build
+
 set -e
 PFX=$(dirname -- "`stack path --local-pkg-db`")
 LIBWXC=$(ls -- $PFX/lib/*-ghc-*/wxc-*/libwxc.so)||true
