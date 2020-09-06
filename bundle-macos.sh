484d31158bcdcff2d8579ff8da5b0720e440b63f
TGass <tilmann.gass@googlemail.com>
2020-09-06 08:59:37 +0200

    Fix bundle script, Add bundle readme.txt

---
+++ bundle-macos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -x
+
+WORK_DIR=`mktemp -d`
+
+# check if tmp dir was created
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+
+BUNDLE_DIR="macbeth-macos"/`stack path --dist-dir`"/build/Macbeth.app"
+
+stack build --stack-yaml=stack-bundle-macos.yaml
+
+# prepare dmg contents
+cp -r $BUNDLE_DIR $WORK_DIR
+
+# copy LICENSE
+# cp LICENSE $WORK_DIR
+cp macbeth-macos/resources/README.txt $WORK_DIR
+
+xattr -cr $WORK_DIR
+
+# cleanup
+find $WORK_DIR -iname .DS_Store -delete
+
+-hdiutil create -volname Macbeth -srcfolder $WORK_DIR -ov -format UDZO Macbeth.dmg
