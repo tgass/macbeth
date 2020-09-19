c22fe91a14a8b876f04f75e453f5d128cc8804d0
TGass <tilmann.gass@googlemail.com>
2020-09-19 11:09:34 +0200

    Adjournments #11 - UI Resume adjourned game

--- macbeth-wx/src/Macbeth/Wx/Stored.hs
+++ macbeth-wx/src/Macbeth/Wx/Stored.hs
@@ -10,10 +10,12 @@ import           Macbeth.Fics.Api.Stored
 import           Macbeth.Fics.Message hiding (gameId)
 import qualified Macbeth.Wx.Commands as Cmds
 import           Macbeth.Wx.Utils
+import           Safe
 import           System.IO
 
 data CtxMenu = CtxMenu {
-    refresh :: MenuItem ()
+    resume :: MenuItem ()
+  , refresh :: MenuItem ()
 }
 
 widget :: Handle -> Panel () -> IO (ListCtrl (), Message -> IO ())
@@ -28,26 +30,31 @@ widget h p = do
       , ("Next", AlignLeft, -1)
       , ("Date", AlignLeft, -1)
       ]]
+
   --listCtrlSetColumnWidths storedListCtrl 100
   set storedListCtrl [on listEvent := onListEvent storedListCtrl h]
 
   storedMenuPane <- menuPane []
   storedMenuPopup <- ctxMenu storedMenuPane
 
-  listItemRightClickEvent storedListCtrl $ \evt -> do
-    pt <- listEventGetPoint evt
-    menuPopup storedMenuPane pt storedListCtrl
-
   set (refresh storedMenuPopup) [on command := Cmds.stored h]
-  return (storedListCtrl, handler storedListCtrl storedMenuPopup storedVar)
+
+  listItemRightClickEvent storedListCtrl $ \evt -> do
+    idx <- listEventGetIndex evt
+    set (resume storedMenuPopup) [on command := do
+      vals <- readTVarIO storedVar
+      maybe (return ()) (Cmds.match h . sOponent) $ vals `atMay` idx
+     ]
+    listEventGetPoint evt >>= flip (menuPopup storedMenuPane) storedListCtrl
+
+  return (storedListCtrl, handler storedListCtrl storedVar)
 
 
-handler :: ListCtrl () -> CtxMenu -> TVar [Stored] -> Message -> IO ()
-handler storedListCtrl ctx storedVar cmd = case cmd of
+handler :: ListCtrl () -> TVar [Stored] -> Message -> IO ()
+handler storedListCtrl storedVar cmd = case cmd of
   StoredGames stored -> do
     atomically $ modifyTVar storedVar $ const stored
     set storedListCtrl [items := toList <$> stored]
-
   _ -> return ()
 
 
@@ -58,11 +65,12 @@ onListEvent listCtrl h evt = case evt of
 
 
 ctxMenu :: Menu () -> IO CtxMenu
-ctxMenu ctxMenu = CtxMenu <$> menuItem ctxMenu [ text := "Refresh" ]
+ctxMenu ctxMenu = CtxMenu 
+  <$> menuItem ctxMenu [ text := "Resume" ]
+  <*> menuItem ctxMenu [ text := "Refresh" ]
 
 
 toList :: Stored -> [String]
 toList Stored{..} = [show sId, show sColor, sOponent, show sOn, sType, sNext, sDate]
 
 
-storedDummy = [Stored 1 White "TheDane" False "br  2  12" "0-0" "B2" "???" "Sun Nov 23,  6:14 CST 1997"]
